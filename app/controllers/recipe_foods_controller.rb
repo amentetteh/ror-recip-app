b7b380db2b02a7fca4c66b465ad57968199bbd25
SimonGideon <simongideon918@gmail.com>
2023-03-29 20:49:40 +0300

    Public recipe route update

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -3,7 +3,7 @@ class RecipeFoodsController < ApplicationController
     @user = User.includes(:recipes, :foods)
     @food_counts = @user.map { |user| [user.name, user.foods.count] }
     @food_prices = @user.map { |user| [user.name, user.foods.sum(:price)] }
-    @recipes = Recipe.includes(:recipe_foods, :foods).order('created_at DESC')
+    @recipes = Recipe.includes(:recipe_foods, :foods).where(public: true).order('created_at DESC')
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :foods, only: [:index, :show, :new, :create, :update, :destroy]
-  resources :recipe_foods, only: [:index, :show, :new, :create, :update, :destroy]
+  resources :recipe_foods, path: 'public_foods', only: [:index, :show, :new, :create, :update, :destroy]
 end

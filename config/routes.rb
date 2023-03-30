94285313fc486841795ce4028489950fadd5f315
Simon Gideon <simongideon918@gmail.com>
2023-03-30 19:54:43 +0300

    Revert "Public recipe list"

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  resources :foods, only: [:index, :show, :new, :create, :update, :destroy]
-  resources :recipes, only: [:index, :show, :new, :create, :update, :destroy], path: 'public_recipes' do
-    put 'public', on: :member, to: 'recipes#public_allocation'
-  end
+  resources :foods, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end

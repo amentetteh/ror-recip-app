5efee6b4c2e402607d1e1ed29a5e23ca515352d5
SimonGideon <simongideon918@gmail.com>
2023-03-31 13:56:16 +0300

    Linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,24 +1,25 @@
 class RecipesController < ApplicationController
-  def index
-    @users = User.includes(recipes: %i[recipe_foods foods]).includes(:foods)
-    @food_counts = @users.map { |user| [user.name, user.foods.count] }
-    @food_prices = @users.map { |user| [user.name, user.foods.sum(:price)] }
-    @recipes = Recipe.includes(:recipe_foods, :foods).where(public: true).order('created_at DESC')
+    def index
+      @user = User.includes(:recipes, :foods)
+      @food_counts = @user.map { |user| [user.name, user.foods.count] }
+      @food_prices = @user.map { |user| [user.name, user.foods.sum(:price)] }
+      @recipes = Recipe.includes(:recipe_foods, :foods).where(public: true).order('created_at DESC')
+    end
+  
+    def show
+      @recipe = Recipe.includes(:foods).find(params[:id])
+    end
+  
+    def public_allocation
+      @recipe = Recipe.find(params[:id])
+      @recipe.update(public: params[:recipe][:public])
+      redirect_to @recipe
+    end
+  
+    def destroy_from_form
+      @food = Food.find(params[:food_id])
+      @food.destroy
+      redirect_to foods_path(current_user)
+    end
+    
   end
-
-  def show
-    @recipe = Recipe.includes(:foods).find(params[:id])
-  end
-
-  def public_allocation
-    @recipe = Recipe.find(params[:id])
-    @recipe.update(public: params[:recipe][:public])
-    redirect_to @recipe
-  end
-
-  def destroy_from_form
-    @food = Food.find(params[:food_id])
-    @food.destroy
-    redirect_to foods_path(current_user)
-  end
-end

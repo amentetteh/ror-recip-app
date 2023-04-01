165d60a350bf71fa6ad4b4aa31cea647a522d3e6
Amen TETTEH <tkamen@outlook.com>
2023-04-01 12:25:53 +0000

    Finalize styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :foods, only: [:index, :show, :new, :create, :update, :destroy]
-  # resources :recipes, only: [:index, :show, :new, :create, :update, :destroy], path: 'public_recipes' do
-  #   put 'public', on: :member, to: 'recipes#public_allocation'
-  # end
+
   resources :foods
   resources :recipes do
     resources :recipe_foods
